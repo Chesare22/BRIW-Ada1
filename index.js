bb96efa2254f5ae8f9ae8a53750d7920bff6b9b3
César González <chesarglez429@gmail.com>
2023-08-17 13:48:29 -0600

    :sparkles: Add search bar and make a request

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log('Hello world')
+const Queries = (() => {
+  const baseUrl = 'https://es.wikipedia.org/w/api.php'
+  const baseParams = Object.freeze({
+    srlimit: '500',
+    srqiprofile: 'classic',
+    srinfo: 'totalhits',
+    action: 'query',
+    list: 'search',
+    format: 'json',
+    origin: '*',
+    srprop: [
+      'size',
+      'timestamp',
+      'snippet',
+    ].join('|'),
+  })
+
+  const search = (srsearch, sroffset = 0) => {
+    const params = new URLSearchParams({
+      ...baseParams,
+      srsearch,
+      sroffset,
+    })
+    return fetch(`${baseUrl}?${params}`)
+      .then(response => response.json())
+  }
+
+  return Object.freeze({ search })
+})()
+
+
+const searchInput = document.getElementById('search-input')
+const searchButton = document.getElementById('search-button')
+
+
+searchButton.addEventListener('click', () => {
+  Queries.search(searchInput.value).then(console.log)
+})
