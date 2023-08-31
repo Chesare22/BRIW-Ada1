16add6a0e590f4f19eba7bb0b8c60b6276e314d4
César González <chesarglez429@gmail.com>
2023-08-31 15:35:05 -0600

    :lipstick: Show date and size of articles and change page style

--- modules/View.js
+++ modules/View.js
@@ -8,8 +8,9 @@ const View = (() => {
 
   const hideErrorMessage = () => showErrorMessage('')
 
+  const dateFormatter = new Intl.DateTimeFormat('es-MX', { dateStyle: 'full' })
 
-  const articleToHtmlElement = ({ title, pageid, snippet }) => {
+  const articleToHtmlElement = ({ title, pageid, snippet, size, date }) => {
     const titleElement = document.createElement('a')
     titleElement.setAttribute('href', `https://es.wikipedia.org/?curid=${pageid}`)
     titleElement.setAttribute('class', 'article-title')
@@ -19,9 +20,19 @@ const View = (() => {
     snippetElement.setAttribute('class', 'article-snippet')
     snippetElement.innerHTML = snippet
 
+    const dateElement = document.createElement('p')
+    dateElement.setAttribute('class', 'article-metadata')
+    dateElement.innerText = `Última modificación: ${dateFormatter.format(date)}`
+
+    const sizeElement = document.createElement('p')
+    sizeElement.setAttribute('class', 'article-metadata')
+    sizeElement.innerText = `${size} Bytes`
+
     const containerElement = document.createElement('div')
     containerElement.appendChild(titleElement)
     containerElement.appendChild(snippetElement)
+    containerElement.appendChild(dateElement)
+    containerElement.appendChild(sizeElement)
     return containerElement
   }
 
