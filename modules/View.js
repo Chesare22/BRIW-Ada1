7eb43192dd9200ab5f8e9914eb047619620015f2
César González <chesarglez429@gmail.com>
2023-08-31 00:54:59 -0600

    :lipstick: Change typography of the results

--- modules/View.js
+++ modules/View.js
@@ -12,9 +12,11 @@ const View = (() => {
   const articleToHtmlElement = ({ title, pageid, snippet }) => {
     const titleElement = document.createElement('a')
     titleElement.setAttribute('href', `https://es.wikipedia.org/?curid=${pageid}`)
+    titleElement.setAttribute('class', 'article-title')
     titleElement.innerText = title
 
     const snippetElement = document.createElement('p')
+    snippetElement.setAttribute('class', 'article-snippet')
     snippetElement.innerHTML = snippet
 
     const containerElement = document.createElement('div')
